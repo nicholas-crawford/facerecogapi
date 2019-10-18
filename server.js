140b05ca0727c60587efa8e29249bdac83550a57
Nicholas Crawford <5213659+nicholas-crawford@users.noreply.github.com>
2019-10-18 18:35:41 +1100

    Fixed host

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const postgresDB = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-perpendicular-28784',
-      user : 'postgres',
-      password : 'test',
-      database : 'faceRecogDB'
+      host : process.env.DATABASE_URL,
+      ssl: true,
     }
   });
 
